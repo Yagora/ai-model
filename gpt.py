a1ab5649bf99060c020934f21b1614a4fb6de31b
Yagora <tanguy.charon47@gmail.com>
2023-11-13 17:42:10 +0100

    feat(gpt): tensor the datas

--- gpt.py
+++ gpt.py
@@ -30,6 +30,11 @@ itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
-print(encode("DoNow "))
-print(decode(encode("DoNow")))
+print(encode("First"))
+print(decode(encode("First")))
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000])
 
